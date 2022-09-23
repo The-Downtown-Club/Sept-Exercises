3a2a5a704e0abb93215b020be0a7f2eda8c092d3
Vijay Raj Jain <rajjainvijay5@gmail.com>
2022-09-24 01:58:17 +0530

    API ready for review

--- week_2_api_dev/flask_api/database/session.py
+++ week_2_api_dev/flask_api/database/session.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from config import DATABASE_URI
+from .config import DATABASE_URI
 
 
 db_engine = create_engine(DATABASE_URI)
